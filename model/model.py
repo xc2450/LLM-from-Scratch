483733463eddd1f88271973e139bf1dda4daa712
xc2450git config --global user.name xc2450 <xi.chen.4211@gmail.com>
2025-12-02 16:29:32 -0500

    Add rotary position embedding and feedforward implementation

--- model/model.py
+++ model/model.py
@@ -105,8 +105,9 @@ class FeedForward(torch.nn.Module):
         output = self.down_proj(self.act_fn(self.up_proj(hidden_states)) * self.gate_proj(hidden_states))
         return self.dropout(output)
 
-def precompute_freqs_cis(dim: int, end: int = int(32*1024), rope_theta: float = 1e6, rope_scaling: Optional[dict] = None):
-    freqs, attn_factor = 1.0 / (rope_theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim)), 1.0
+def precompute_freqs_cis(dim: int, end: int = int(32 * 1024), rope_base: float = 1e6,
+                         rope_scaling: Optional[dict] = None):
+    freqs, attn_factor = 1.0 / (rope_base ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim)), 1.0
     if rope_scaling is not None:
         orig_max, factor, beta_fast, beta_slow, attn_factor = (
             rope_scaling.get("original_max_position_embeddings", 2048), rope_scaling.get("factor", 16),
@@ -114,7 +115,7 @@ def precompute_freqs_cis(dim: int, end: int = int(32*1024), rope_theta: float = 
         )
         if end / orig_max > 1.0:
             # YaRN: f'(i) = f(i)((1-γ) + γ/s), where γ∈[0,1] is linear ramp
-            inv_dim = lambda b: (dim * math.log(orig_max / (b * 2 * math.pi))) / (2 * math.log(rope_theta))
+            inv_dim = lambda b: (dim * math.log(orig_max / (b * 2 * math.pi))) / (2 * math.log(rope_base))
             low, high = max(math.floor(inv_dim(beta_fast)), 0), min(math.ceil(inv_dim(beta_slow)), dim // 2 - 1)
             ramp = torch.clamp((torch.arange(dim // 2, device=freqs.device).float() - low) / max(high - low, 0.001), 0, 1)
             freqs = freqs * (1 - ramp + ramp / factor)
@@ -125,7 +126,8 @@ def precompute_freqs_cis(dim: int, end: int = int(32*1024), rope_theta: float = 
     freqs_sin = torch.cat([torch.sin(freqs), torch.sin(freqs)], dim=-1) * attn_factor
     return freqs_cos, freqs_sin
 
-def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     def rotate_half(x):
         return torch.cat((-x[..., x.shape[-1] // 2:], x[..., : x.shape[-1] // 2]), dim=-1)
 
